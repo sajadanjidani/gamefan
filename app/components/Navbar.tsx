ba39d3fab932a818dcedd3e9b842807d7723b500
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-27 14:55:22 +0330

    create Navbar.tsx file

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from "react"
+
+export default class Navbar extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+} 
