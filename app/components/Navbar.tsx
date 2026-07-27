492906c286c321450cd7599b9d7764e976dda89c
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-27 15:14:05 +0330

    coding logo in Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,9 +10,13 @@ export default class Navbar extends React.Component {
         return(
             <div className="w-full h-15 mt-10">
                 {/* logo */}
-                <div>
-                    <img alt="logoIcon" src={''}/>
-                    <h1></h1>
+                <div className="flex items-center">
+                    <img className="w-15 h-15" alt="logoIcon" src={'./images/logo.png'}/>
+                    <h1 className="text-3xl font-bold">
+                        <span className="text-blue-400">Ga</span>
+                        <span className="text-purple-400">me</span>
+                        <span className="text-purple-500">Fan</span>
+                    </h1>
                 </div>
                 {/* list */}
                 <ul>
