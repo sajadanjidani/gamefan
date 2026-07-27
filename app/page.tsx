3e52fec23c4bc38a9b43dacffb1b1f996fe8f3a8
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-27 17:36:20 +0330

    import Header component in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 
 // import component
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div className="container mx-auto">
       <Navbar />
+      <Header />
     </div>
   );
 }
