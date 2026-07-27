bff1f23c3b34aa56f1c1174bbb03041706ea8224
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-27 14:58:49 +0330

    import Navbar component in page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+// import component
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="container mx-auto">
+      <Navbar />
+    </div>
   );
 }
