37819dad5c647d17329ae8b9741aafb2e42d4bd1
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 20:58:42 +0330

    coding responsive ADS component

--- app/components/ADS.tsx
+++ app/components/ADS.tsx
@@ -8,19 +8,23 @@ export default class ADS extends React.Component {
 
     render(){
         return(
-            <section className="container mx-auto md:h-85 sm:h-200 h-170 overflow-hidden md:rounded-r-4xl bg-linear-90 from-blue-950/20 from-50% to-background flex md:flex-row flex-col-reverse xl:px-0 px-5">
+            <section className="container mx-auto md:h-85 sm:h-200 h-170 overflow-hidden md:rounded-r-4xl flex md:flex-row flex-col-reverse xl:px-0 px-5">
                 {/* image box */}
-                <div className="xl:w-3/10 lg:w-4/10 md:w-4/10 w-full sm:h-full h-4/10 "></div>
+                <div className="xl:w-3/10 lg:w-4/10 md:w-4/10 w-full sm:h-full h-4/10 flex justify-center items-end ">
+                    <img alt="imageADS" src='./images/ADS/ADSImage.webp'/>
+                </div>
                 {/* content */}
-                <div className="xl:w-7/10 lg:w-6/10 md:w-6/10 w-full sm:h-full h-6/10 flex justify-center items-center md:**:text-left **:text-center md:**:mx-0 **:mx-auto">
-                    <div className="grid justify-center items-center xl:gap-5 lg:gap-3 sm:gap-5 xl:mt-0 md:-mt-12 sm:mt-0 -mt-10 w-9/10">
-                        <h3 className="font-bold text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, repellat!</h3>
-                        <p className="leading-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta magni dolores qui ut modi vel cupiditate in non explicabo placeat voluptatem eius quas sunt fugit inventore soluta reprehenderit aliquam at repudiandae blanditiis excepturi, facilis aut. Dolorum rem maxime numquam deserunt!</p>
+                <div className="relative xl:w-7/10 lg:w-6/10 md:w-6/10 w-full sm:h-full h-6/10 flex justify-center items-center md:**:text-left **:text-center md:**:mx-0 **:mx-auto">
+                    <div className="grid justify-center items-center xl:gap-5 lg:gap-3 sm:gap-5 xl:-mt-10 md:-mt-12 sm:mt-0 -mt-10 w-9/10">
+                        <h3 className="font-bold text-xl">Buy all kinds of gift cards and other items on the Gamefan site</h3>
+                        <p className="leading-7">Lorem Ipsum is a fabricated text with the production of incomprehensible simplicity from the printing industry and using graphic designers. Printers and texts, but also newspapers and magazines in columns and rows as necessary and for the current conditions of technology required and diverse applications aimed at improving practical tools. Many books in sixty-three percent of the past, present and future require a lot of knowledge of society and specialists</p>
                         <div className="relative md:px-0 px-17">
-                            <button className="flex justify-center items-center w-35 h-12 md:left-auto -left-0 rounded-xl absolute mt-3 bg-linear-90 from-purple-600 from-30% to-blue-600 hover:cursor-pointer">Show All Items</button>
+                            <button className="flex justify-center items-center w-35 h-12 md:left-auto -left-0 rounded-xl absolute mt-3 bg-linear-90 from-purple-600 from-30% to-blue-600 hover:cursor-pointer">View all items</button>
                             <div className="w-35 h-12 rounded-xl absolute blur-md -z-10 -left-0.5 mt-3 bg-linear-90 from-purple-600 from-30% to-blue-600"></div>
                         </div>
                     </div>
+                    {/* bg Image */}
+                    <img alt="backgroundImage" src='./images/ADS/ADSbgImage.jpg' className="w-full h-full object-fill opacity-10 absolute -z-10 sm:-mt-3"/>
                 </div>
             </section>
         )
