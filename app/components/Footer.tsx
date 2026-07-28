34daddd21813b6afd85979f90b815e3499d494a3
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 22:16:15 +0330

    coding responsive Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -23,24 +23,24 @@ export default class Footer extends React.Component {
                 </div>
                 {/* content */}
                 <div className="w-full h-full flex justify-center items-center">
-                    <div className="w-5/10 h-7/10 flex flex-col">
+                    <div className="h-7/10 flex flex-col">
                         <div className="flex justify-center items-center">
                             <img className="w-15 h-15" alt="logoImage" src='./images/logo.png' />
-                            <h2 className="-ml-10 font-bold lg:text-3xl md:text-lg sm:text-xl text-xl">
+                            <h2 className="-ml-10 font-bold text-3xl">
                                 <span className="text-blue-400">Ga</span>
                                 <span className="text-purple-400">me</span>
                                 <span className="text-purple-500">Fan</span>
                             </h2>
                         </div>
                         <div>
-                            <ul className="flex mt-5 justify-center items-center">
-                                <li className="border-r-2 px-4 border-white hover:cursor-pointer hover:text-blue-400">Support</li>
-                                <li className="border-r-2 px-4 border-white hover:cursor-pointer hover:text-blue-400">contact us</li>
-                                <li className="border-r-2 px-4 border-white hover:cursor-pointer hover:text-blue-400">Terms and conditions of use</li>
-                                <li className="px-4 hover:cursor-pointer hover:text-blue-400">Frequently asked questions</li>
+                            <ul className="grid grid-cols-2 grid-rows-2 gap-3 sm:flex mt-5 justify-center items-center text-center sm:text-left">
+                                <li className="border-r-2 px-4 border-white hover:cursor-pointer hover:text-blue-400 md:text-base text-sm">Support</li>
+                                <li className="sm:border-r-2 px-4 border-white hover:cursor-pointer hover:text-blue-400 md:text-base sm:text-sm text-xs">contact us</li>
+                                <li className="border-r-2 px-4 border-white hover:cursor-pointer hover:text-blue-400 md:text-base text-sm">Terms and conditions of use</li>
+                                <li className="px-4 hover:cursor-pointer hover:text-blue-400 md:text-base sm:text-sm text-xs">Frequently asked questions</li>
                             </ul>
                         </div>
-                        <div className="flex justify-center items-center gap-10 my-10 *:size-7 *:hover:cursor-pointer">
+                        <div className="flex justify-center items-center sm:gap-10 gap-5 my-10 *:size-7 *:hover:cursor-pointer">
                             <SlSocialInstagram className="hover:text-purple-400"/>
                             <FaFacebook className="hover:text-blue-700"/>
                             <FaTwitter className="hover:text-blue-400"/>
@@ -49,8 +49,8 @@ export default class Footer extends React.Component {
                             <FaWhatsapp className="hover:text-green-400"/>
                         </div>
                         <div className="text-center">
-                            <p>This is a practice project created by <a className="text-blue-400 hover:text-purple-400" href="https://github.com/sajadanjidani">me</a>.</p>
-                            <p className="mt-2">Create in 2026/7/28</p>
+                            <p className="text-xs">This is a practice project created by <a className="text-blue-400 hover:text-purple-400" href="https://github.com/sajadanjidani">me</a>.</p>
+                            <p className="mt-2 text-xs">Create in 2026/7/28</p>
                         </div>
                     </div>
                 </div>
