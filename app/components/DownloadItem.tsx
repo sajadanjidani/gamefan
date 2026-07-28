a8b1f34f1570b0178752c5abb02481e28d7bc3cd
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 14:19:45 +0330

    create DownloadItem component

---
+++ app/components/DownloadItem.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from "react"
+
+export default class DownloadItem extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <></>
+        )
+    }
+}
