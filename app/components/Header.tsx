3b43859680f7e4048d93d6f7942e38c24c20b83c
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 13:36:21 +0330

    finish coding Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,7 +9,7 @@ export default class Header extends React.Component {
 
         this.state = {
             CategoryInfo : [
-                {id : 1 , title : 'FBS'},
+                {id : 1 , title : 'Shooting'},
                 {id : 2 , title : 'Action'},
             ]
         }
@@ -31,7 +31,7 @@ export default class Header extends React.Component {
                             <div className="flex justify-end items-center gap-3">
                                 {this.state.CategoryInfo.map(category => <CategoryBtn key={category.id} {...category} /> )}
                             </div>
-                            <p className="leading-7 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nemo velit vero aliquid, culpa odio dolore quae molestiae facere nisi aliquam ratione perferendis magnam consequuntur tenetur, non dolorum placeat est. Iure fugit magnam corporis? Dolores!</p>
+                            <p className="leading-7 mt-3">A novice mercenary fighting for survival in a brutal reality, one day this hero, by carrying out one of the orders, acquires a chip that hides the secret of immortality, and with this he intends to become one of the most powerful cyberpunks. We can create the main character, such as gender, appearance, origin of the character, and provide additional options for the mission.</p>
                             <div className="relative">
                                 <button className="w-30 h-12 rounded-xl absolute z-10 mt-3 bg-linear-90 from-purple-600 from-30% to-blue-600 hover:cursor-pointer">Download</button>
                                 <div className="w-30 h-12 rounded-xl absolute blur-md -left-0.5 mt-3 bg-linear-90 from-purple-600 from-30% to-blue-600"></div>
