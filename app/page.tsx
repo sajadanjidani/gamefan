ca012b84494e406723ba4ae3233370736bbdde5b
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 13:58:28 +0330

    import DownloadBox in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 // import component
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import DownloadBox from "./components/DownloadBox";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Header />
+      <DownloadBox />
     </div>
   );
 }
