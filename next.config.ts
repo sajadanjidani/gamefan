08ff076c8fa537c5a5f7f41ee02167877fd36ec1
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-29 00:08:33 +0330

    fix github pages css path

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
   basePath: "/gamefan",
-  assetPrefix: "/gamefan/",
 };
 
 export default nextConfig;
