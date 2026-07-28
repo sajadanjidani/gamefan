8a948bf3990bbafa3f3e4c9c6f37903bda0ff135
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 23:44:16 +0330

    DownloadItem.tsx change js to ts

--- app/components/DownloadItem.tsx
+++ app/components/DownloadItem.tsx
@@ -1,17 +1,26 @@
 'use client'
-import React from "react"
 
-export default class DownloadItem extends React.Component {
-    constructor(props){
-        super(props)
-    }
+import React from "react";
 
-    render(){
-        return(
-            <div className="lg:w-full lg:h-full sm:w-full h-full flex justify-center items-center gap-3 px-5 rounded-3xl border-2 border-b-blue-400 border-t-purple-500 border-l-blue-400 border-r-purple-500">
-                <img alt="logoCategory" src={this.props.imageSrc} className="w-16 h-14"/>
-                <p className="font-bold">{this.props.title}</p>
-            </div>
-        )
-    }
+type DownloadItemProps = {
+  imageSrc: string;
+  title: string;
+};
+
+export default class DownloadItem extends React.Component<DownloadItemProps> {
+  render() {
+    return (
+      <div className="lg:w-full lg:h-full sm:w-full h-full flex justify-center items-center gap-3 px-5 rounded-3xl border-2 border-b-blue-400 border-t-purple-500 border-l-blue-400 border-r-purple-500">
+        <img
+          alt="logoCategory"
+          src={this.props.imageSrc}
+          className="w-16 h-14"
+        />
+
+        <p className="font-bold">
+          {this.props.title}
+        </p>
+      </div>
+    );
+  }
 }
