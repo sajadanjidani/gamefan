8c03d1d1a1d9a7d7bf0060a042ef6222133b7b97
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 14:21:05 +0330

    coding DownloadItem component

--- app/components/DownloadItem.tsx
+++ app/components/DownloadItem.tsx
@@ -8,7 +8,9 @@ export default class DownloadItem extends React.Component {
 
     render(){
         return(
-            <></>
+            <div className="w-full h-full rounded-3xl border-2 border-b-blue-400 border-t-purple-500 border-l-blue-400 border-r-purple-500">
+                
+            </div>
         )
     }
 }
