6d21404e612b80259ebaac11d474a37f04f87bae
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 23:44:48 +0330

    DownloadBox.tsx change js to ts

--- app/components/DownloadBox.tsx
+++ app/components/DownloadBox.tsx
@@ -1,28 +1,60 @@
 'use client'
-import React from "react"
-import DownloadItem from "./DownloadItem"
 
-export default class DownloadBox extends React.Component {
-    constructor(props){
-        super(props)
+import React from "react";
+import DownloadItem from "./DownloadItem";
 
-        this.state = {
-            DownloadItemInfo : [
-                {id : 1, title : 'Windows Games', imageSrc : './images/categoryLogos/windowsLogo.png'},
-                {id : 2, title : 'Android  Games', imageSrc : './images/categoryLogos/androidLogo.png'},
-                {id : 3, title : 'PlayStation Games', imageSrc : './images/categoryLogos/PlayStationLogo.png'},
-                {id : 4, title : 'Xbox Games', imageSrc : './images/categoryLogos/xboxLogo.png'},
-            ]
-        }
-    }
+type DownloadItemInfo = {
+  id: number;
+  title: string;
+  imageSrc: string;
+};
 
-    render(){
-        return(
-            <div className="lg:w-full sm:w-9/10 lg:h-50 sm:h-60 w-8/10 h-130 flex justify-center items-center mx-auto my-10">
-                <div className="w-9/10 lg:h-25 h-full sm:gap-10 gap-5 sm:mt-0 mt-10 grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1">
-                    {this.state.DownloadItemInfo.map(infoItem => <DownloadItem key={infoItem.id} {...infoItem}/>)}
-                </div>
-            </div>
-        )
-    }
+type DownloadBoxState = {
+  DownloadItemInfo: DownloadItemInfo[];
+};
+
+export default class DownloadBox extends React.Component<{}, DownloadBoxState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      DownloadItemInfo: [
+        {
+          id: 1,
+          title: "Windows Games",
+          imageSrc: "./images/categoryLogos/windowsLogo.png",
+        },
+        {
+          id: 2,
+          title: "Android Games",
+          imageSrc: "./images/categoryLogos/androidLogo.png",
+        },
+        {
+          id: 3,
+          title: "PlayStation Games",
+          imageSrc: "./images/categoryLogos/PlayStationLogo.png",
+        },
+        {
+          id: 4,
+          title: "Xbox Games",
+          imageSrc: "./images/categoryLogos/xboxLogo.png",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="lg:w-full sm:w-9/10 lg:h-50 sm:h-60 w-8/10 h-130 flex justify-center items-center mx-auto my-10">
+        <div className="w-9/10 lg:h-25 h-full sm:gap-10 gap-5 sm:mt-0 mt-10 grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1">
+          {this.state.DownloadItemInfo.map((infoItem) => (
+            <DownloadItem
+              key={infoItem.id}
+              {...infoItem}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
