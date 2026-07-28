4b17d759c1557d201485333b926a1037c6002d08
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 23:47:12 +0330

    SliderItem.tsx change js to ts

--- app/components/Slider/SliderItem.tsx
+++ app/components/Slider/SliderItem.tsx
@@ -1,17 +1,31 @@
 'use client'
-import React from "react"
 
-export default class SliderItem extends React.Component {
-    constructor(props){
-        super(props)
-    }
+import React from "react";
 
-    render(){
-        return(
-            <div className="min-w-45 h-full mb-10 rounded-3xl relative overflow-hidden shadow-lg shadow-purple-950">
-                <img alt="backgroundImage" src={this.props.imageSrc} className="w-full h-full object-fill absolute -z-10"/>
-                <p className="text-center font-bold mt-50 mb-3">{this.props.title}</p>
-            </div>
-        )
-    }
+
+type SliderItemProps = {
+  imageSrc: string;
+  title: string;
+};
+
+
+export default class SliderItem extends React.Component<SliderItemProps> {
+
+  render() {
+    return (
+      <div className="min-w-45 h-full mb-10 rounded-3xl relative overflow-hidden shadow-lg shadow-purple-950">
+
+        <img
+          alt="backgroundImage"
+          src={this.props.imageSrc}
+          className="w-full h-full object-fill absolute -z-10"
+        />
+
+        <p className="text-center font-bold mt-50 mb-3">
+          {this.props.title}
+        </p>
+
+      </div>
+    );
+  }
 }
