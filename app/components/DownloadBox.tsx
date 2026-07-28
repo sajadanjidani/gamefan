806d92bbf9464f9dfed47dcc03ed2cbf86c190c0
sajadanjidani <sajadanjiidanii@gmail.com>
2026-07-28 13:57:40 +0330

    create DownloadBox class component

---
+++ app/components/DownloadBox.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from "react"
+
+export default class DownloadBox extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
